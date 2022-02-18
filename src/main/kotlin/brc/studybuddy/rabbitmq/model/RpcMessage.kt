f9d5f69d2980aec04ad7bc9dd5588436748db6ad
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-18 17:14:40 +0100

    Refactor RabbitMq sub-module

--- src/main/kotlin/brc/studybuddy/rabbitmq/model/RpcMessage.kt
+++ src/main/kotlin/brc/studybuddy/rabbitmq/model/RpcMessage.kt
@@ -1,20 +1,11 @@
 package brc.studybuddy.rabbitmq.model
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import brc.studybuddy.rabbitmq.serializeData
 import com.rabbitmq.client.AMQP.BasicProperties
 import reactor.rabbitmq.RpcClient.RpcRequest
 
 class RpcMessage<T>(
-    properties: BasicProperties?,
-    body: T
+    properties: BasicProperties?, body: T
 ) : RpcRequest(properties, serializeData(body)) {
     constructor(body: T) : this(null, body)
-
-    companion object {
-        private val objectMapper = ObjectMapper()
-
-        private fun <T> serializeData(value: T): ByteArray =
-            objectMapper.writeValueAsString(value).toByteArray(Charsets.UTF_8)
-
-    }
 }
