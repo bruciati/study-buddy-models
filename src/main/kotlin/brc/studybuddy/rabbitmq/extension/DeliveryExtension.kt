8df483ea4fa51e087e514844cc755840354e0473
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-23 22:24:08 +0100

    Remove RabbitMq models

--- src/main/kotlin/brc/studybuddy/rabbitmq/extension/DeliveryExtension.kt
+++
@@ -1,6 +0,0 @@
-package brc.studybuddy.rabbitmq.extension
-
-import brc.studybuddy.rabbitmq.deserializeData
-import com.rabbitmq.client.Delivery
-
-fun <T> Delivery.getBodyClass(bodyType: Class<T>): T = deserializeData(this.body, bodyType)
