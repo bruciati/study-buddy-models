b7d590705d641ced76fd707b48e9083eb449e7e0
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-01 15:26:29 +0100

    Add WebClient support functions

--- src/main/kotlin/brc/studybuddy/webclient/extension/RequestBodyUriSpec.kt
+++ src/main/kotlin/brc/studybuddy/webclient/extension/RequestBodyUriSpec.kt
@@ -5,8 +5,18 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.RequestBodyUriSpec
 import reactor.core.publisher.Mono
 
-fun RequestBodyUriSpec.bodyGraphQl(
+fun RequestBodyUriSpec.graphQlBody(
     query: String,
     variables: HashMap<String, Any>? = null
 ): WebClient.RequestHeadersSpec<*> =
-    this.body(Mono.just(GraphQlRequest("{ $query }", variables)), GraphQlRequest::class.java)
+    this.body(Mono.just(GraphQlRequest(query, variables)), GraphQlRequest::class.java)
+
+fun RequestBodyUriSpec.graphQlQuery(
+    query: String,
+    variables: HashMap<String, Any>? = null
+): WebClient.RequestHeadersSpec<*> = this.graphQlBody("query { $query }", variables)
+
+fun RequestBodyUriSpec.graphQlMutation(
+    query: String,
+    variables: HashMap<String, Any>? = null
+): WebClient.RequestHeadersSpec<*> = this.graphQlBody("mutation { $query }", variables)
